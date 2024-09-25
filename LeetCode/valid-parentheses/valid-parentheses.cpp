653a17c80f37a1a7303db2f353b55e0d48987972
AndrewBarthUSMC <andyjacobbarth@gmail.com>
2024-09-24 22:33:46 -0400

    Modified valid-parentheses.cpp to include bitmasking

--- LeetCode/valid-parentheses/valid-parentheses.cpp
+++ LeetCode/valid-parentheses/valid-parentheses.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool isValid(string s) {
+            if (s.length() & 1 == 1) {
+                return false;
+            }
+
         unordered_map<char, char> brackets {
             {'(', ')'},
             {'[', ']'},
